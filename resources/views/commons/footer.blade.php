d8399bc2eba636c2af3121dbcdbb76eff3027ae4
sosuke iwabuchi <ii.iwabuchi@gmail.com>
2017-03-21 20:52:00 +0900

    基本修正

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,6 +1,11 @@
 @section('footer')
     @yield('addJs')
-    <script src="{{{asset('js/vue.js?').str_random(5)}}}"></script>
+    <script src="{{{asset('assets/js/vue.js?').str_random(5)}}}"></script>
+    <script src="{{{asset('assets/js/jquery-3.2.0.js?').str_random(5)}}}"></script>
+    <script src="{{{asset('assets/bootstrap/js/bootstrap.min.js?').str_random(5)}}}"></script>
+    <script>
+        $("#loading-bg, #loading").hide();
+    </script>
     @yield('endJs')
   </body>
 </html>
--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -4,8 +4,17 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{{ csrf_token() }}}">
     <title> {{{ getTitle() }}}</title>
+    <link rel="stylesheet" type="text/css" href="{{{asset('assets/css/common.css?').str_random(5)}}}">
+    <link rel="stylesheet" type="text/css" href="{{{asset('assets/bootstrap/css/bootstrap.min.css?').str_random(5)}}}">
     @yield('addCss')
   </head>
+  <div id="loading-bg">
+      <div id="loading">
+          <img src="{{{ asset('assets/images/ajax-loader.gif') }}}" >
+      </div>
+  </div>
 @stop
